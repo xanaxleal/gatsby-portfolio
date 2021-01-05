fdce67fc2bd126affc2e8dd3acf1846d37980261
Ana Leal <analealdom@gmail.com>
2021-01-05 16:22:52 +0100

    Header fix

--- src/components/Info.js
+++ src/components/Info.js
@@ -3,15 +3,15 @@ import infoStyles from "./Info.module.css"
 
 const Info = (props) => (
     <nav className={infoStyles.project__information}>
-      <h3 className={infoStyles.menu__item}>{props.title}</h3>
-      <ul className={infoStyles.project__details}>
-        <li className={infoStyles.project__details_discipline}>Discipline:  {props.discipline}</li>
-        <li className={infoStyles.project__details_sector}>Client:  {props.sector}</li>
-        <li className={infoStyles.project__details_year}>Year:  {props.year}</li>
-        {props.isCollaboration === "true" ? <li className={infoStyles.project__details_collaboration}>This project was made in collaboration with <a href={props.collaboratorUrl}>{props.collaborator}</a></li> : ""}
-        {props.isAwarded === "true" ? <li className={infoStyles.project__details_awarded}>Awards <a href={props.awardUrl}>{props.award}</a></li> : ""}
+      <h3 className="title">{props.title}</h3>
+      <ul className="text">
+        <li className="text">Discipline:  {props.discipline}</li>
+        <li className="text">Client:  {props.sector}</li>
+        <li className="text">Year:  {props.year}</li>
+        {props.isCollaboration === "true" ? <li className="text">This project was made in collaboration with <a href={props.collaboratorUrl}>{props.collaborator}</a></li> : ""}
+        {props.isAwarded === "true" ? <li className="text">Awards <a href={props.awardUrl}>{props.award}</a></li> : ""}
       </ul>
-      <div className={infoStyles.project__details_description} dangerouslySetInnerHTML={{__html: props.content}} />
+      <div className={`${infoStyles.project__details_description} text`} dangerouslySetInnerHTML={{__html: props.content}} />
     </nav>
 )
 
