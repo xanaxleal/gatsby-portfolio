e86b2c30fdaa11d08ed1cff89b3248fb799c6c72
Ana Leal <analealdom@gmail.com>
2021-01-05 15:12:21 +0100

    Bg interactivity fix

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,21 +1,11 @@
 import React from "react";
 import rowStyles from "./Row.module.css"
 
-const Row = (props) => {
-    function bgShow (event){
-        event.target.parentElement.parentElement.style.backgroundImage = `url("../img/${event.target.dataset.image}")`;
-        Array.from(event.target.parentElement.parentElement.children).forEach(el => el.classList.add('transparent'));
-        event.target.parentElement.classList.remove('transparent');
-        event.target.parentElement.style.borderTop = '1px solid white';
-    }
-    function bgHide(event){
-        event.target.parentElement.parentElement.style.backgroundImage = 'none';
-        Array.from(event.target.parentElement.parentElement.children).forEach(el => el.classList.remove('transparent'));
-        event.target.parentElement.style.borderTop = 'none';
-    }
-    let last_known_scroll_position = 0;
-    let ticking = false;
-    if (typeof window !== `undefined`) {
+class Row extends React.Component {
+    componentDidMount () {
+        let last_known_scroll_position = 0;
+        let ticking = false;
+        
         function handleScroll(scroll_pos) {
             document.querySelector('main').style.backgroundPositionY = (scroll_pos - document.querySelector('.header').offsetHeight) + "px";
         }
@@ -33,18 +23,32 @@ const Row = (props) => {
         }
         });
     }
+    render () {
+        function bgShow (event){
+            event.target.parentElement.parentElement.style.backgroundImage = `url("../img/${event.target.dataset.image}")`;
+            Array.from(event.target.parentElement.parentElement.children).forEach(el => el.classList.add('transparent'));
+            event.target.parentElement.classList.remove('transparent');
+            event.target.parentElement.style.borderTop = '1px solid white';
+        }
     
-    
-    return <article
-    onMouseEnter={bgShow}
-    onMouseLeave={bgHide}>
-        <a href={props.slug} className={rowStyles.menu__row} id={props.order} data-image={props.image}>
-        <p className={`${rowStyles.menu__item} text`}>{props.discipline}</p>
-        <h3 className={`${rowStyles.menu__item} title`}>{props.title}</h3>
-        <p className={`${rowStyles.menu__item} text`}>{props.sector}</p>
-        <p className={`${rowStyles.menu__item} text`}>{props.year}</p>
-        </a>
-    </article>  
-}
-
-export default Row;
+        function bgHide(event){
+            event.target.parentElement.parentElement.style.backgroundImage = 'none';
+            Array.from(event.target.parentElement.parentElement.children).forEach(el => el.classList.remove('transparent'));
+        }
+          
+      return (
+        <article
+        onMouseEnter={bgShow}
+        onMouseLeave={bgHide}>
+            <a href={this.props.slug} className={rowStyles.menu__row} id={this.props.order} data-image={this.props.image}>
+            <p className={`${rowStyles.menu__item} text`}>{this.props.discipline}</p>
+            <h3 className={`${rowStyles.menu__item} title`}>{this.props.title}</h3>
+            <p className={`${rowStyles.menu__item} text`}>{this.props.sector}</p>
+            <p className={`${rowStyles.menu__item} text`}>{this.props.year}</p>
+            </a>
+        </article>
+      )
+    }
+  }
+  
+  export default Row;
